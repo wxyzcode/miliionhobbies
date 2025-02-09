d069c658478353a562b0af3a061355653346ed90
wxyzcode <mikhailcernikin@gmail.com>
2025-02-09 19:14:22 +0700

    Update search_results_beta.py

--- search_results_beta.py
+++ search_results_beta.py
@@ -3,29 +3,20 @@ import json
 
 app = Flask(__name__)
 
-# Загружаем данные из JSON-файла
-with open('data.json', 'r', encoding='utf-8') as f:
+with open('hobby_data/data.json', 'r', encoding='utf-8') as f:
     hobbies_data = json.load(f)
-
-@app.route('/')
-def index():
-    return render_template('search.html')
 
 @app.route('/search_results')
 def search_results():
-    return render_template('search_results.html')
-
-@app.route('/api/search')
-def api_search():
     query = request.args.get('query', '').lower()
-    results = []
-
-    # Поиск по данным
-    for hobby in hobbies_data.values():
+    result = []
+    n = 0
+    for hobby in hobbies_data['hobbies']:
         if query in hobby['name'].lower() or query in hobby['desc'].lower():
-            results.append(hobby)
+            n += 1
+            result.append(hobby)
 
-    return jsonify(results)
-  
+    return render_template('search_results.html', result=result, n=n)
+
 if __name__ == '__main__':
     app.run(debug=True)
