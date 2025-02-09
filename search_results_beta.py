2f35948455b4bfcff46defa046360bd07799e51e
wxyzcode <mikhailcernikin@gmail.com>
2025-02-09 21:53:23 +0700

    Update search_results_beta.py

--- search_results_beta.py
+++ search_results_beta.py
@@ -6,17 +6,22 @@ app = Flask(__name__)
 with open('hobby_data/data.json', 'r', encoding='utf-8') as f:
     hobbies_data = json.load(f)
 
-@app.route('/search_results')
+@app.route('/search')
+def render():
+    return render_template('search.html')
+
+@app.route('/search_results', methods=["GET"])
 def search_results():
     query = request.args.get('query', '').lower()
-    result = []
+    results = []
     n = 0
     for hobby in hobbies_data['hobbies']:
-        if query in hobby['name'].lower() or query in hobby['desc'].lower():
+        if query in hobby['title'].lower() or query in hobby['desc'].lower():
             n += 1
-            result.append(hobby)
+            results.append(hobby)
 
-    return render_template('search_results.html', result=result, n=n)
+    return render_template('search_results.html', results=results, n=n)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
