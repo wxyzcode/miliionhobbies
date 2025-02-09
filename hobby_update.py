a7af81a8ec3c9be969e51e6cec4243ed97cfeb49
wxyzcode <mikhailcernikin@gmail.com>
2025-02-10 00:02:08 +0700

    Update hobby_update.py

--- hobby_update.py
+++ hobby_update.py
@@ -3,24 +3,35 @@ import json
 
 app = Flask(__name__)
 
-with open('hobby_data/data.json', 'r', encoding='utf-8') as f:
-    hobbies_data = json.load(f)
-
 @app.route('/search')
 def render():
     return render_template('search.html')
 
-@app.route('/search_results', methods=["GET"])
+@app.route('/create_hobby_result', methods=["GET"])
 def search_results():
-    query = request.args.get('query', '').lower()
-    results = []
-    n = 0
-    for hobby in hobbies_data['hobbies']:
-        if query in hobby['title'].lower() or query in hobby['desc'].lower():
-            n += 1
-            results.append(hobby)
-
-    return render_template('search_results.html', results=results, n=n)
+    with open('hobby_data/data.json', 'r', encoding='utf-8') as f:
+        hobbies_data = json.load(f)
+        
+    name = request.args.get('hobby_name')
+    desc = request.args.get('hobby_desc')
+    link = request.args.get('hobby_link')
+    user = request.args.get('hobby_user')
+    uniqueuser = request.args.get('hobby_uniqueuser')
+    
+    new_data ={
+            "title": name,
+            "desc": desc,
+            "uniquename": uniqueuser,
+            "name": user,
+            "link": link
+         }
+    
+    hobbies_data.append(new_data)
+    
+    with open('hobby_data/data.json', 'w') as f:
+        json.dump(hobbies_data, f, indent=4)
+    
+    return render_template('create_hobby_result.html', results=results, n=n)
 
 if __name__ == '__main__':
     app.run(debug=True)
